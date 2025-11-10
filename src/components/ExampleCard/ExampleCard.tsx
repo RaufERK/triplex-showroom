45b9af0c810f1dc21703e08ac8eddef33b7e5ac5
Rauf Erk <erk.rauf@gmail.com>
2025-11-10 16:01:40 +0300

    chore: update ExampleCard component

--- src/components/ExampleCard/ExampleCard.tsx
+++ src/components/ExampleCard/ExampleCard.tsx
@@ -1,8 +1,18 @@
-import { Text, Caption, ETextSize, EFontType, ECaptionSize } from '@sberbusiness/triplex-next'
+import {
+  Text,
+  Caption,
+  ETextSize,
+  EFontType,
+  ECaptionSize,
+} from '@sberbusiness/triplex-next'
 import { type ExampleCardProps } from '../../types'
 import styles from './ExampleCard.module.css'
 
-export const ExampleCard = ({ title, description, children }: ExampleCardProps) => (
+export const ExampleCard = ({
+  title,
+  description,
+  children,
+}: ExampleCardProps) => (
   <div className={styles.card}>
     <div className={styles.header}>
       <Text size={ETextSize.B2} type={EFontType.PRIMARY}>
@@ -17,4 +27,3 @@ export const ExampleCard = ({ title, description, children }: ExampleCardProps) 
     <div className={styles.body}>{children}</div>
   </div>
 )
-
