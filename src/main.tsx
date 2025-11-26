a3f9788f7697da1000d28f7950c6a71f324259f7
Rauf Erk <erk.rauf@gmail.com>
2025-11-26 15:36:45 +0300

    Добавлена страница с иконками 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
-import { StrictMode, type RefObject } from 'react';
-import { createRoot } from 'react-dom/client';
-import '@sberbusiness/triplex-next/styles/triplex-next.css';
-import './index.css';
-import { ThemeProvider, ETriplexNextTheme } from '@sberbusiness/triplex-next';
-import App from './App';
+import { StrictMode, type RefObject } from 'react'
+import { createRoot } from 'react-dom/client'
+import '@sberbusiness/triplex-next/styles/triplex-next.css'
+import '@sberbusiness/icons-next/styles/icons.css'
+import './index.css'
+import { ThemeProvider, ETriplexNextTheme } from '@sberbusiness/triplex-next'
+import App from './App'
 
-const themeScopeRef = { current: document.documentElement } as RefObject<HTMLElement>;
+const themeScopeRef = {
+  current: document.documentElement,
+} as RefObject<HTMLElement>
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider scopeRef={themeScopeRef} theme={ETriplexNextTheme.LIGHT}>
       <App />
     </ThemeProvider>
-  </StrictMode>,
-);
+  </StrictMode>
+)
