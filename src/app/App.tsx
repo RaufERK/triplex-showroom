d474c4262dc5f6efe19b6726ded59ac3a04256be
Rauf Erk <erk.rauf@gmail.com>
2025-11-26 15:25:02 +0300

    Добавлена страница с иконками

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,54 +1,67 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import { Title, Text, ETitleSize, ETextSize, EFontType } from '@sberbusiness/triplex-next'
+import {
+  ThemeProvider as IconsThemeProvider,
+  EIconsTheme,
+} from '@sberbusiness/icons-next'
+import {
+  Title,
+  Text,
+  ETitleSize,
+  ETextSize,
+  EFontType,
+} from '@sberbusiness/triplex-next'
 import { HomePage } from '../pages/home'
 import { ColorsPage } from '../pages/colors'
 import { IconsPage } from '../pages/icons'
 import styles from './App.module.css'
 import '../styles/global.css'
-import '@sberbusiness/icons/css/icons.css'
+
+const AnyIconsThemeProvider = IconsThemeProvider as any
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className={styles.app}>
-        <aside className={styles.sidebar}>
-          <div className={styles.sidebarHeader}>
-            <Title size={ETitleSize.H2} tag='h1'>
-              Triplex Next UI Kit
-            </Title>
-            <Text size={ETextSize.B3} type={EFontType.SECONDARY}>
-              Демонстрация ключевых компонентов дизайн-системы Triplex Next в контексте React + TypeScript.
-            </Text>
-          </div>
-          <nav className={styles.nav}>
-            <Link to='/' className={styles.navLink}>
-              <Text tag='span' size={ETextSize.B3} type={EFontType.PRIMARY}>
-                🏠 Главная
+      <AnyIconsThemeProvider theme={EIconsTheme.LIGHT}>
+        <div className={styles.app}>
+          <aside className={styles.sidebar}>
+            <div className={styles.sidebarHeader}>
+              <Title size={ETitleSize.H2} tag='h1'>
+                Triplex Next UI Kit
+              </Title>
+              <Text size={ETextSize.B3} type={EFontType.SECONDARY}>
+                Демонстрация ключевых компонентов дизайн-системы Triplex Next в
+                контексте React + TypeScript.
               </Text>
-            </Link>
-            <Link to='/colors' className={styles.navLink}>
-              <Text tag='span' size={ETextSize.B3} type={EFontType.PRIMARY}>
-                🎨 Цвета
-              </Text>
-            </Link>
-            <Link to='/icons' className={styles.navLink}>
-              <Text tag='span' size={ETextSize.B3} type={EFontType.PRIMARY}>
-                🔷 Иконки
-              </Text>
-            </Link>
-          </nav>
-        </aside>
-        <main className={styles.content}>
-          <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/colors' element={<ColorsPage />} />
-            <Route path='/icons' element={<IconsPage />} />
-          </Routes>
-        </main>
-      </div>
+            </div>
+            <nav className={styles.nav}>
+              <Link to='/' className={styles.navLink}>
+                <Text tag='span' size={ETextSize.B3} type={EFontType.PRIMARY}>
+                  🏠 Главная
+                </Text>
+              </Link>
+              <Link to='/colors' className={styles.navLink}>
+                <Text tag='span' size={ETextSize.B3} type={EFontType.PRIMARY}>
+                  🎨 Цвета
+                </Text>
+              </Link>
+              <Link to='/icons' className={styles.navLink}>
+                <Text tag='span' size={ETextSize.B3} type={EFontType.PRIMARY}>
+                  🔷 Иконки
+                </Text>
+              </Link>
+            </nav>
+          </aside>
+          <main className={styles.content}>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='/colors' element={<ColorsPage />} />
+              <Route path='/icons' element={<IconsPage />} />
+            </Routes>
+          </main>
+        </div>
+      </AnyIconsThemeProvider>
     </BrowserRouter>
   )
 }
 
 export default App
-
--- src/pages/icons/IconsPage.tsx
+++ src/pages/icons/IconsPage.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {
   Title,
   Text,
@@ -7,18 +8,33 @@ import {
   ECaptionSize,
   EFontType,
 } from '@sberbusiness/triplex-next'
+// import { BookkeeperblackAccIcon64 } from '@sberbusiness/icons/BookkeeperblackAccIcon64'
 import { Section, ExampleCard } from '../../components'
-import { NodocumentsMrkIcon96, ConfirmloginMrkIcon128 } from '@sberbusiness/icons-next'
+import {
+  NodocumentsMrkIcon96,
+  ConfirmloginMrkIcon128,
+  GigaassistaintOtherBrdIcon20,
+  BalanceStrokeNavIcon20,
+  CardcreditmirMkrIcon32,
+  TouchidMrkIcon96,
+} from '@sberbusiness/icons-next'
+import * as Icons from '@sberbusiness/icons-next'
 import styles from './IconsPage.module.css'
 
+const ICONS_BLACKLIST = new Set(['ThemeProvider', 'useTheme', 'EIconsTheme'])
+
 export const IconsPage = () => {
+  const iconEntries = Object.entries(Icons)
+    .filter(([name]) => !ICONS_BLACKLIST.has(name))
+    .sort(([a], [b]) => a.localeCompare(b))
+
   return (
     <div className={styles.page}>
       <div className={styles.hero}>
         <Title size={ETitleSize.H1}>Иконки @sberbusiness/icons-next</Title>
         <Text size={ETextSize.B1} type={EFontType.SECONDARY}>
-          Небольшая галерея живых иконок из библиотеки @sberbusiness/icons-next и минимальные примеры
-          их подключения в React + Triplex Next.
+          Небольшая галерея живых иконок из библиотеки @sberbusiness/icons-next
+          и минимальные примеры их подключения в React + Triplex Next.
         </Text>
       </div>
 
@@ -45,6 +61,64 @@ export const IconsPage = () => {
               ConfirmloginMrkIcon128
             </Caption>
           </div>
+
+          <div className={styles.iconCard}>
+            <div className={styles.iconWrapper}>
+              <CardcreditmirMkrIcon32 />
+            </div>
+            <Caption size={ECaptionSize.C1} className={styles.iconName}>
+              CardcreditmirMkrIcon32
+            </Caption>
+          </div>
+
+          <div className={styles.iconCard}>
+            <div className={styles.iconWrapper}>
+              <GigaassistaintOtherBrdIcon20 paletteIndex={5} />
+            </div>
+            <Caption size={ECaptionSize.C1} className={styles.iconName}>
+              GigaassistaintOtherBrdIcon20
+            </Caption>
+          </div>
+
+          <div className={styles.iconCard}>
+            <div className={styles.iconWrapper}>
+              <BalanceStrokeNavIcon20 paletteIndex={5} />
+            </div>
+            <Caption size={ECaptionSize.C1} className={styles.iconName}>
+              BalanceStrokeNavIcon20
+            </Caption>
+          </div>
+
+          <div className={styles.iconCard}>
+            <div className={styles.iconWrapper}>
+              <TouchidMrkIcon96 />
+            </div>
+            <Caption size={ECaptionSize.C1} className={styles.iconName}>
+              TouchidMrkIcon96
+            </Caption>
+          </div>
+        </div>
+      </Section>
+
+      <Section
+        id='all-icons'
+        title='Все иконки из @sberbusiness/icons-next'
+        description='Полная галерея экспортируемых иконок из библиотеки.'
+      >
+        <div className={styles.iconsGrid}>
+          {iconEntries.map(([name, IconComponent]) => {
+            const Icon = IconComponent as any
+            return (
+              <div key={name} className={styles.iconCard}>
+                <div className={styles.iconWrapper}>
+                  {React.createElement(Icon, { paletteIndex: 0 })}
+                </div>
+                <Caption size={ECaptionSize.C1} className={styles.iconName}>
+                  {name}
+                </Caption>
+              </div>
+            )
+          })}
         </div>
       </Section>
 
